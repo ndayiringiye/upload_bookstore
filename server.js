dfa202cbc2c4e564e36bfa873dbcff350a8d56d5
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-05-16 09:46:28 +0200

    chore:/ imporoving setup

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hello")
+import express from "express";
+const app = express();
+
+app.use(express.json());
+app.use("uploads", express.static("uploads"));
+
+app.get("/", (req, res)=>{
+    res.send("you're rich");
+});
+app.listen(5000, ()=>{
+    console.log("server is running on port 5000")
+});
